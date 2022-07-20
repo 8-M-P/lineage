8f9de7b25b3cda7d14903b097239c78222898bf3
Burak Mete Perçin <bmpbilisim@gmail.com>
2022-07-20 21:27:13 +0300

    initial files with rev2

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,7 @@
-from django.urls import path
+from django.contrib.auth.decorators import login_required
+from django.urls import path, re_path
+from django.conf import settings
+from django.views.static import serve
 from .views import *
 
 urlpatterns = [
@@ -12,3 +15,12 @@ urlpatterns = [
     path('giris/', login_page, name='login_page'),
     path('cikis/', logout_page, name='logout'),
 ]
+
+
+@login_required
+def protected_serve(request, path, document_root=None, show_indexes=False):
+    return serve(request, path, document_root, show_indexes)
+
+
+urlpatterns += [re_path(r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:], protected_serve,
+                        {'document_root': settings.MEDIA_ROOT})]
--- lineage/urls.py
+++ lineage/urls.py
@@ -1,19 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth.decorators import login_required
-from django.urls import path, include, re_path
-from django.conf import settings
-from django.views.static import serve
+from django.urls import path, include
 
 urlpatterns = [
     path('', include('core.urls'), name='core'),
     path('admin/', admin.site.urls),
 ]
-
-
-@login_required
-def protected_serve(request, path, document_root=None, show_indexes=False):
-    return serve(request, path, document_root, show_indexes)
-
-
-urlpatterns += [re_path(r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:], protected_serve,
-                        {'document_root': settings.MEDIA_ROOT})]
